7e4898772ba3c0a49fa087d84e1b84687db798af
German Escobar <german.escobarc@gmail.com>
2017-01-22 11:55:16 -0500

    Configure Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar inverse staticTop>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Ecommerce</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+
 
       </div>
     );
